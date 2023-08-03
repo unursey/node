35c0b0d3802ebe03252d676ee591830279dc3b50
anna <unursey@gmail.com>
2023-08-04 01:36:25 +0300

    add package

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const newObj = require('mpackage');
-
-const obj = {
-  name: 'тОни стАрк',
-  dateOfBirth: '29.05.1970',
-  purpose: 'сПасТи Мир и Умереть'
-};
-
-console.log(newObj(obj));
+console.log('Hello NPM');
